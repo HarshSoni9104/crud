07384bdfb73e4ab45298f5f58a115d3d4d826cb8
Harsh Soni <soniharsh9104@gmail.com>
2025-11-23 21:42:54 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { CommonModule } from '@angular/common';
+    import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent , CommonModule , ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+    import { Routes } from '@angular/router';
+    import { LoginComponent } from './login/login.component';
+    import { AuthGuard } from './auth.guard';
+    import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
-export const routes: Routes = [];
+    export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'add-employee',
+    component: AddEmployeeComponent,
+    canActivate: [AuthGuard]
+  }
+];
+
